e5f2f2cd852ce33cbf5c7c02a4a87c56e6d3eac7
Anurag <115922618+Anurag340@users.noreply.github.com>
2023-08-14 14:33:01 +0530

    Add files via upload

---
+++ misc1.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int z=4;
+	    int maxi=0;
+	    while(z--){
+	        int x;
+	        cin>>x;
+	        if(x>=maxi){
+	            maxi=x;
+	        }
+	        
+	        
+	    }
+	    cout<<maxi<<endl;
+	}
+	return 0;
+}
